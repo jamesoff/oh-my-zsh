703c8387f7f919b09dce90eaedd9ba7ee1cab54f
James Seward <james.seward@razorfish.com>
2013-11-22 16:08:37 +0000

    Move reset stuff to a function Make the reset commands more option-like Use clear/reset and synonyms for the same thing

--- plugins/ec2/ec2.plugin.zsh
+++ plugins/ec2/ec2.plugin.zsh
@@ -5,28 +5,39 @@ function ec2() {
 		return 0
 	fi
 
-	if [ "$1" = "clear" ]; then
-		echo Clearing EC2 profile.
-		unset AWS_CREDENTIAL_FILE
-		unset AWS_DEFAULT_REGION
-		unset AWS_ACCESS_KEY_ID
-		unset AWS_SECRET_ACCESS_KEY
-		unset AWS_DEFAULT_OUTPUT
-		
-		return 0
-	fi
-
 	if [ "x$1" = "x" ]; then
 		echo Available ec2 profiles:
 		ls -1 ~/.ec2
 		return 0
 	fi
 
+	if [ "$1" = "--clear" ]; then
+		ec2_reset
+		return 0
+	fi
+
+	if [ "$1" = "--reset" ]; then
+		ec2_reset
+		return 0
+	fi
+
+
 	if [ -f ~/.ec2/$1 ]; then
+		#[[ $(stat -f%SMp "$1") =~ .w.$ ]] && echo "Error: $1 is group-writable" && return 1
+		#[[ $(stat -f%SLp "$1") =~ .w.$ ]] && echo "Error: $1 is world-writable" && return 1
 		echo Switching EC2 profile to $1.
 		source ~/.ec2/$1
 	else
 		echo Profile not defined.
 		return 1
 	fi
+}
+
+function ec2_reset() {
+	echo Clearing EC2 profile.
+	unset AWS_CREDENTIAL_FILE
+	unset AWS_DEFAULT_REGION
+	unset AWS_ACCESS_KEY_ID
+	unset AWS_SECRET_ACCESS_KEY
+	unset AWS_DEFAULT_OUTPUT
 }
